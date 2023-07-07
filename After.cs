27c2701d288ef8dec37edb20e518e8ebdbd567e7
PashaahsaP <ppsairda@yandex.ru>
2023-07-07 15:30:45 +0700

    really add

--- After.cs
+++ After.cs
@@ -1,60 +1,116 @@
-﻿using System;
+﻿using NUnit.Framework.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Windows.Input;
 
 namespace LimitedSizeStack;
 
 public class ListModel<TItem>
 {
+	
+	public Invoker<TItem> Invoker { get; set; }
     public List<TItem> Items { get; set; }
-    public LimitedSizeStack<(int index,(bool op, TItem value))> History { get; set; }//(index,(append/remove,value))
     public int UndoLimit;
-        
+
+    #region ctor
     public ListModel(int undoLimit) : this(new List<TItem>(), undoLimit)
-    {
-    }
+	{
+	}
 
-    public ListModel(List<TItem> items, int undoLimit)
-    {
+	public ListModel(List<TItem> items, int undoLimit)
+	{
+		UndoLimit = undoLimit;
         Items = items;
-        UndoLimit = undoLimit;
-        History = new LimitedSizeStack<(int, (bool, TItem))>(undoLimit);
+        Invoker = new Invoker<TItem>(UndoLimit);
     }
 
-    public void AddItem(TItem item)
-    {
-        History.Push((Items.Count,(true,item)));
-        Items.Add(item);
-    }
+    #endregion    
 
-    public void RemoveItem(int index)
-    {
-        History.Push((index, (false, Items[index])));
-        Items.RemoveAt(index);
-    }
+	public void AddItem(TItem item)
+	{
+        Invoker.SetCommand(new AddCommand<TItem>(item));
+        Invoker.Run(Items);
+	}
 
-    public bool CanUndo()
-    {
-        return History.Count > 0;
-    }
+	public void RemoveItem(int index)
+	{
+        Invoker.SetCommand(new RemoveAtCommand<TItem>(index));
+        Invoker.Run(Items);
+	}
 
-    public void Undo()
-    {
+	public bool CanUndo()
+	{
+		return Invoker.History.Count> 0;
+	}
+
+	public void Undo()
+	{
         if (CanUndo())
         {
-            var temp = History.Pop();
-            if (temp.Item2.op)
-            {
-                Items.RemoveAt(temp.Item1);
-            }
-            else
-            {
-                var newList = new List<TItem>();
-                newList.AddRange(Items);
-                newList.Add(default(TItem));
-                newList.Insert(temp.index,temp.Item2.value);
-                newList.RemoveAt(newList.Count-1);
-                Items = newList;
-            }
+            var command = Invoker.History.Pop();
+            Invoker.SetCommand(command);
+            Invoker.Cancel(Items);
         }
+	}
+}
+
+public class Invoker<T>  
+{
+    public LimitedSizeStack<Icommand<T>> History { get; set; }
+    public Icommand<T> command { get; set; }
+    public Invoker(int size)
+    {
+        History = new LimitedSizeStack<Icommand<T>>(size);
     }
+
+    public void SetCommand(Icommand<T> c) => command = c;
+    public void Run(List<T> Items)
+    {
+        command.Do(Items);
+        History.Push(command);
+    }
+    public void Cancel(List<T> Items)
+    {
+        command.Undo(Items);
+
+    }
+}
+public class AddCommand<T>:Icommand<T>
+{
+    private T item;
+
+    public AddCommand(T item) =>this.item = item;
+
+
+    public void Do(List<T> Items)
+    {
+        Items.Add(item);
+    }
+    public void Undo(List<T> Items)
+    {
+        Items.Remove(item);
+    }
+
+}
+public class RemoveAtCommand<T>:Icommand<T>
+{
+    public T Item { get; set; }
+    private int index;
+    public RemoveAtCommand( int index) => this.index = index;
+    public void Do(List<T> Items)
+    {
+        Item = Items[index];
+        Items.RemoveAt(index);
+    }
+    public void Undo(List<T> Items)
+    {
+        Items.Add(Item);
+        Items.Insert(index, Item);
+        Items.RemoveAt(Items.Count-1);
+    }
+}
+public interface Icommand<Q>
+{
+    public void Do(List<Q> Items);
+    public void Undo(List<Q> Items);
 }
